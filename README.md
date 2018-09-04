59c304892db4ab7c7944c1369d0af4acdeb77299
ddaaggeett <david.g.daggett@gmail.com>
2018-09-04 09:57:56 -0400

    single file OR every file conversion

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 #### convert your JSON file data to a scannable QR code
 
+![jsonToQR .gif](https://media.giphy.com/media/830OB3YZqmWpExxayG/giphy.gif)
+
+single file:
+
     node . <name>
+
+or every json file in `./json/`
+
+    node .
 
 place json files here: `./json/<name>.json`
 find generated images here: `./codes/<name>.png`
--- index.js
+++ index.js
@@ -2,13 +2,40 @@ var fs = require('fs')
 var path = require('path')
 var QRCode = require('qrcode')
 
-const name = process.argv[2]
-const outQRFile = name.concat('.png')
-const jsonFile = name.concat('.json')
-const jsonString = fs.readFileSync(path.join('json',jsonFile)).toString()
-const outFilePath = path.join('codes', outQRFile)
+const getFileName = (file) => {
+    var name = file
+    var cut = name.indexOf('.json')
+    if(cut != -1) {
+        name = name.substring(0, cut)
+    }
+    return name
+}
 
-QRCode.toFile(outFilePath, jsonString, (err) => {
-    if (err) throw err
-    console.log('\nnew QR file @ ' + outFilePath + '\n')
-})
+const runAllFiles = () => {
+    fs.readdirSync('./json/').forEach(file => {
+        if(file.includes('.json')) {
+            const jsonString = fs.readFileSync(path.join('json',file)).toString()
+            const fileName = getFileName(file)
+            const outQRFile = fileName.concat('.png')
+            const outFilePath = path.join('codes', outQRFile)
+            QRCode.toFile(outFilePath, jsonString, (err) => {
+                if (err) throw err
+                console.log('new QR file @ ' + outFilePath)
+            })
+        }
+    })
+}
+
+const runSingleFile = (name) => {
+    const outQRFile = name.concat('.png')
+    const jsonFile = name.concat('.json')
+    const jsonString = fs.readFileSync(path.join('json',jsonFile)).toString()
+    const outFilePath = path.join('codes', outQRFile)
+    QRCode.toFile(outFilePath, jsonString, (err) => {
+        if (err) throw err
+        console.log('\nnew QR file @ ' + outFilePath)
+    })
+}
+
+if(process.argv[2] == null) runAllFiles()
+else runSingleFile(process.argv[2])
